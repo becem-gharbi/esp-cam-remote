696ba8e732e0f1fff7517cd168ac4618e90c4b1e
becem-gharbi <becem.gharbi@live.com>
2024-03-12 08:53:07 +0100

    refactor(app): consider device status on ViewerTracker

--- app/components/ViewerTracker.vue
+++ app/components/ViewerTracker.vue
@@ -3,10 +3,25 @@
 </template>
 
 <script setup lang="ts">
+const { $mqtt, $device, $config } = useNuxtApp()
+
+const visibility = ref(true)
+
 onMounted(() => {
-  updateViewer(true)
   document.addEventListener('visibilitychange', () => {
-    updateViewer(!document.hidden)
+    visibility.value = !document.hidden
   })
 })
+
+const viewer = computed(() => visibility.value && $device.connected.value)
+
+watch(viewer, updateViewer, { immediate: true })
+
+function updateViewer (state: boolean) {
+  /* eslint-disable no-console */
+  console.log('ℹ️ 󠀠viewer status', state)
+  const topic = `device/${$config.public.device.id}/command/custom`
+  const message = JSON.stringify({ viewer: state })
+  $mqtt.client.publish(topic, message, { qos: 2, retain: false })
+}
 </script>
